57ba138f476d938bc787eef8a3dfd077e9a1697d
vbroussard <vbroussard@users.noreply.github.com>
2014-03-17 14:56:48 +0000

    ADVERTDASH-1 : creation of module announce mydashboard
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/collaboration/module-announce-mydashboard@64924 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/announce/modules/mydashboard/service/MyAnnoucesMyDashboardComponent.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.announce.modules.mydashboard.service;
+
+import fr.paris.lutece.plugins.announce.web.AnnounceApp;
+import fr.paris.lutece.plugins.mydashboard.service.MyDashboardComponent;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.message.SiteMessageException;
+import fr.paris.lutece.portal.service.util.AppLogService;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * MyDashboard component to display the list of announces of a user
+ */
+public class MyAnnoucesMyDashboardComponent extends MyDashboardComponent
+{
+    private static final String MYDASHBOARD_COMPONENT_ID = "announce-mydashboard.myAnnouncesDashboardComponent";
+
+    private static final String MESSAGE_MY_ANNOUNCES_MYDASHBOARD_COMPONENT_DESCRIPTION = "module.announce.mydashboard.myAnnouncesDashboardComponent.description";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDashboardData( HttpServletRequest request )
+    {
+        try
+        {
+            return AnnounceApp.getManageUserAnnounces( request );
+        }
+        catch ( SiteMessageException e )
+        {
+            // We catch site messages since they tell the user to log in if he has not logged in
+            AppLogService.error( e.getMessage( ), e );
+        }
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getComponentId( )
+    {
+        return MYDASHBOARD_COMPONENT_ID;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getComponentDescription( Locale locale )
+    {
+        return I18nService.getLocalizedString( MESSAGE_MY_ANNOUNCES_MYDASHBOARD_COMPONENT_DESCRIPTION, locale );
+    }
+
+}
